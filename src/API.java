ce30d16e5ddd9a1270c9956afabca084875ba174
Naila Awan <125586761+Naila-Awan@users.noreply.github.com>
2024-03-24 01:38:46 +0500

    Implemented Abstract class API
    Done in meet with the rest of the contributors

---
+++ src/API.java
@@ -0,0 +1,17 @@
+public abstract class API {
+
+        protected double Latitude;
+        protected double Longitude;
+    
+        protected String apiKey;
+    
+        public void setLatitude(double lati){}
+        public void setLongitude(double longi){}
+        
+        public String APIcall(String location){
+            return "";
+        }
+    
+}
+
+
