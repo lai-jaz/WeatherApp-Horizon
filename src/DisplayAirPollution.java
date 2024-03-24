d0ce44626ea790e51f389f3b37552f840217c250
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-03-24 16:50:57 +0500

    Update DisplayAirPollution.java
    AQI Alert -> Fatima
    Fix AirPollution parsing -> Laiba

--- src/DisplayAirPollution.java
+++ src/DisplayAirPollution.java
@@ -10,7 +10,7 @@ import org.json.JSONArray;
 
 
 class AirPollutionAPI extends API
-{//56b4ca79beae47bbe777752bd2dc564d
+{
     private static final String API_KEY = "42c9e7c4ab03739b4d29f144eaef927b";
     private static final String API_URL = "http://api.openweathermap.org/data/2.5/air_pollution?lat=%s&lon=%s&appid=%s";
 
@@ -55,7 +55,6 @@ class AirPollutionAPI extends API
     {
         String API_Geo = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=" + API_KEY;
         try {
-            //String apiUrl = String.format(API_URL, location, API_KEY);
             URL url = new URL(API_Geo);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
@@ -112,70 +111,8 @@ class AirPollutionAPI extends API
             }
         }
 
-        //JSONObject jsonObject = new JSONObject(geoJSON);
-
         this.setLatitude(lat);
         this.setLongitude(longi);
-    }
-}
-
-class AirPollutionInfo {
-    private double AirQualityIndex;
-    private double COperct;
-    private double NOperct;
-    private double N20perct;
-    private double Ozone;
-    private double SO2perct;
-    private double Ammoniaperct;
-    private double pm2_5;
-    private double pm10;
-
-    public AirPollutionInfo(double AirQualityIndex, double COperct, double NOperct, double N20perct, double Ozone, double SO2perct, double Ammoniaperct, double pm2_5, double pm10) {
-        this.AirQualityIndex = AirQualityIndex;
-        this.COperct = COperct;
-        this.NOperct = NOperct;
-        this.N20perct = N20perct;
-        this.Ozone = Ozone;
-        this.SO2perct = SO2perct;
-        this.Ammoniaperct = Ammoniaperct;
-        this.pm2_5 = pm2_5;
-        this.pm10 = pm10;
-    }
-
-    public double getAmmoniaperct() {
-        return Ammoniaperct;
-    }
-
-    public double getOzone() {
-        return Ozone;
-    }
-
-    public double getN20perct() {
-        return N20perct;
-    }
-
-    public double getNOperct() {
-        return NOperct;
-    }
-
-    public double getCOperct() {
-        return COperct;
-    }
-
-    public double getAirQualityIndex() {
-        return AirQualityIndex;
-    }
-
-    public double getpm10() {
-        return pm10;
-    }
-
-    public double getpm2_5() {
-        return pm2_5;
-    }
-
-    public double getSO2perct(){
-        return SO2perct;
     }
 }
 
@@ -200,7 +137,6 @@ public class DisplayAirPollution extends JFrame implements AppHomepage{
         {
             public void actionPerformed(ActionEvent e) {
                 displayInfo();
-                displaySunTimes();
             }
         });
     }
@@ -209,77 +145,55 @@ public class DisplayAirPollution extends JFrame implements AppHomepage{
     public void displayInfo() 
     {
         AirPollutionAPI airPollution_api = new AirPollutionAPI();
-
         String location = locationField.getText();
-
-        String weatherData = airPollution_api.APIcall(location);
-        JSONObject jsonObject = new JSONObject(weatherData);
-        double aqi = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("aqi");
-
-        //Checking the AQI scale for poor or very poor air quality
-        if (aqi == 4 || aqi == 5) {
-        //Display notification
-        JOptionPane.showMessageDialog(this, "Poor air quality detected! AQI: " + aqi, "Air Quality Alert", JOptionPane.WARNING_MESSAGE);
-    }
-
+        String airpolData = airPollution_api.APIcall(location);
 
         // Parse the JSON response
-       /* JSONObject jsonObject = new JSONObject(weatherData);
+       JSONObject jsonObject = new JSONObject(airpolData);
         
-        WeatherInfo obj = getFormData(jsonObject, weatherData);
-        // Display elements from API return on console
-        System.out.println("Visibility: " + obj.getVisibility() + "m");
-        System.out.println("Rain Volume (past 1h): " + obj.getRainVol1h() + "mm");
-        System.out.println("Rain Volume (past 3h): " + obj.getRainVol3h() + "mm");
-        System.out.println("Humidity: " + obj.getHumidity() + "%");
-        System.out.println("Feels like: " + obj.getFeelsLike() + " C");
-        System.out.println("Weather Description: " +obj.getWeatherDescr());
-        System.out.println("Temperature: " + obj.getTemperature() + " C");
+        AirPollutionInfo obj = getFormData(jsonObject, airpolData);
+
+        double Aqi = obj.getAirQualityIndex();
+        double co = obj.getCO();
+        double no =  obj.getNO();
+        double no2 = obj.getNO2();
+        double oz = obj.getOzone();
+        double so2 = obj.getSO2();
+        double ammonia = obj.getAmmonia();
+        double pm2_5 = obj.getpm2_5();
+        double pm10 = obj.getpm10();
 
         // Display elements from API return on console
-        String DisplayInfoGUI = "Visibility: " + obj.getVisibility() + "m\n" 
-                            + "Humidity: " + obj.getHumidity() + "%\n"
-                            + "Feels like: " + obj.getFeelsLike() + " C\n"
-                            + "Weather Description: " + obj.getWeatherDescr() + "\n"
-                            + "Temperature: " + obj.getTemperature() + " C\n"
-                            + "Rain Volume (past 1h): " + obj.getRainVol1h() + "mm\n"
-                            + "Rain Volume (past 3h): " + obj.getRainVol3h() + "mm\n";
-        */
-        weatherOutputArea.append(weatherData);
+        System.out.println("Air Pollution data for " + location);
+        System.out.println("AQI: " + Aqi);
+        System.out.println("Carbon Monoxide: " + co + " μg/m3");
+        System.out.println("Nitrogen Monoxide: " + no + " μg/m3");
+        System.out.println("Nitrogen Dioxide: " + no2 + " μg/m3");
+        System.out.println("Ozone: " + oz + " μg/m3");
+        System.out.println("Sulfur Dioxide: " + so2 + " μg/m3");
+        System.out.println("Ammonia: " + ammonia + " μg/m3");
+        System.out.println("Coarse particulate matter: " + pm10 + " μg/m3");
+        System.out.println("Fine particles matter: " + pm2_5 + " μg/m3");
+        
+        // Display elements from API return on GUI
+        String DisplayInfoGUI = "Air Pollution data for " + location + "\n"
+                                + "AQI: " + Aqi 
+                                + "\nCarbon Monoxide: " + co + " μg/m3"
+                                + "\nNitrogen Monoxide: " + no + " μg/m3"
+                                + "\nOzone: " + oz + " μg/m3"
+                                + "\nSulfur Dioxide: " + so2 + " μg/m3"
+                                + "\nAmmonia: " + ammonia + " μg/m3"
+                                + "\nCoarse particulate matter: " + pm10 + " μg/m3"
+                                + "\nFine particles matter: " + pm2_5 + " μg/m3\n";
+
+        weatherOutputArea.append(DisplayInfoGUI);
+        
+        // AQI Alert
+        if (Aqi == 4 || Aqi == 5) {
+            JOptionPane.showMessageDialog(this, "Poor air quality detected! AQI: " + Aqi, "Air Quality Alert", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
-    private String convertUnixTime(long unixTime) {
-        java.util.Date time = new java.util.Date(unixTime * 1000);
-        return new java.text.SimpleDateFormat("HH:mm:ss").format(time);
-    }
-
-    public void displaySunTimes()
-    {
-        WeatherAPI weather_api = new WeatherAPI();
-
-        String location = locationField.getText();
-        String weatherData = weather_api.APIcall(location);
-
-        // Parse the JSON response
-        JSONObject jsonObject = new JSONObject(weatherData);
-
-        // Retrieve sunrise and sunset times from JSON
-        long sunriseUnix = jsonObject.getJSONObject("sys").getLong("sunrise");
-        long sunsetUnix = jsonObject.getJSONObject("sys").getLong("sunset");
-
-        // Convert Unix time to simple format
-        String sunrise = convertUnixTime(sunriseUnix);
-        String sunset = convertUnixTime(sunsetUnix);
-
-        // Display sunrise and sunset times
-        System.out.println("Sunrise: " + sunrise);
-        System.out.println("Sunset: " + sunset);
-
-        String sunTimesText = "Sunrise: " + sunrise + "\n"
-                            + "Sunset: " + sunset;
-
-        // weatherOutputArea.append(sunTimesText);
-    }
 
     public void displayLocationForm()
     {
@@ -298,40 +212,29 @@ public class DisplayAirPollution extends JFrame implements AppHomepage{
         setVisible(true);
     }
 
-    // make weather info object using the JSONObject
-    public WeatherInfo getFormData(JSONObject jsonObject, String weatherData)
+    // make airpollution info object using the JSONObject
+    public AirPollutionInfo getFormData(JSONObject jsonObject, String weatherData)
     {
-        double temperature = jsonObject.getJSONObject("main").getDouble("temp");
-        double feelsLike = jsonObject.getJSONObject("main").getDouble("feels_like");
-        double humidity = jsonObject.getJSONObject("main").getDouble("humidity");
-        int visibility = jsonObject.getInt("visibility");
-        JSONArray weatherArray = jsonObject.getJSONArray("weather");
-        JSONObject weatherObject = weatherArray.getJSONObject(0);
-        String description = weatherObject.getString("description");
+        double AQI = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("main").getDouble("aqi");
+        double CO = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("co");
+        double NO = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("no");
+        double NO2 = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("no2");
+        double Oz = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("o3");
+        double SO2 = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("so2");
+        double ammonia = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("nh3");
+        double pm2_5 = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("pm2_5");
+        double pm10 = jsonObject.getJSONArray("list").getJSONObject(0).getJSONObject("components").getDouble("pm10");
 
-        double rainVol1h = 0.0;
-        double rainVol3h = 0.0;
-        if(weatherData.contains("\"rain\""))
-        {
-            if(weatherData.contains("\"rain\": {\"1h\":"))
-            {
-                rainVol1h = jsonObject.getJSONObject("rain").getInt("1h");
-            }
-            if(weatherData.contains("\"rain\"") && weatherData.contains("\"3h\":"))
-            {
-                rainVol3h = jsonObject.getJSONObject("rain").getInt("3h");
-            }
-        }
-
-        WeatherInfo obj = new WeatherInfo(temperature, rainVol1h, rainVol3h, visibility, humidity, feelsLike, description);
+        AirPollutionInfo obj = new AirPollutionInfo(AQI, CO, NO, NO2, Oz, SO2, ammonia, pm2_5, pm10);
         return obj;
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new DisplayAirPollution();
             }
         });
     }
+
 }
