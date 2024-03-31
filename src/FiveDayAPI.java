b4a0ce1ed28a8183573b0d2f8b18dbf8b457575d
Naila Awan <125586761+Naila-Awan@users.noreply.github.com>
2024-03-31 22:08:54 +0500

    Implemented Exception Handling for all API outputs

--- src/FiveDayAPI.java
+++ src/FiveDayAPI.java
@@ -42,11 +42,11 @@ public class FiveDayAPI extends API
             
             else 
             {
-                return "Error: Unable to fetch weather data. Response code: " + responseCode;
+                return "Connection error. Response code: " + responseCode;
             }
         } 
         catch (Exception e) {
-            return "Error: " + e.getMessage();
+            return "Error: Unable to fetch five-day weather data. Exception: " + e.getMessage();
         }
     }
 
@@ -74,11 +74,11 @@ public class FiveDayAPI extends API
             
             else 
             {
-                return "Error: Unable to fetch weather data. Response code: " + responseCode;
+                return "Connection error. Response code: " + responseCode;
             }
         } 
         catch (Exception e) {
-            return "Error: " + e.getMessage();
+            return "Error: Unable to fetch location. Exception: " + e.getMessage();
         }
     }
 
@@ -97,23 +97,34 @@ public class FiveDayAPI extends API
     
             if (dateTime.endsWith("12:00:00")) {
 
-                double temperature = forecastItem.getJSONObject("main").getDouble("temp");
-                double feelsLike = forecastItem.getJSONObject("main").getDouble("feels_like");
-                double humidity = forecastItem.getJSONObject("main").getDouble("humidity");
-                int visibility = forecastItem.getInt("visibility");
-                JSONArray weatherArray = forecastItem.getJSONArray("weather");
-                JSONObject weatherObject = weatherArray.getJSONObject(0);
-                String description = weatherObject.getString("description");
+                double temperature = tryExtractDouble(forecastItem,"main","temp");
+                double feelsLike = tryExtractDouble(forecastItem,"main","feels_like");
+                double humidity = tryExtractDouble(forecastItem,"main","humidity");
+
+                //visibility exception handling
+                int visibility;
+                try{
+                    visibility = forecastItem.getInt("visibility");
+                }
+                catch (Exception e)
+                {
+                    visibility = -1;
+                }
+
+                String description =tryExtractString(forecastItem, "description");
+
                 double rainVol1h = 0.0;
                 double rainVol3h = 0.0;
 
-                if (forecastItem.has("rain")) {
-                    JSONObject rainObject = forecastItem.getJSONObject("rain");
-                    if (rainObject.has("1h")) {
-                        rainVol1h = rainObject.getDouble("1h");
+                if(weatherData.contains("\"rain\""))
+                {
+                    if(weatherData.contains("\"rain\": {\"1h\":"))
+                    {
+                        rainVol1h = tryExtractDouble(forecastItem,"rain","1h");
                     }
-                    if (rainObject.has("3h")) {
-                        rainVol3h = rainObject.getDouble("3h");
+                    if(weatherData.contains("\"rain\"") && weatherData.contains("\"3h\":"))
+                    {
+                        rainVol3h = tryExtractDouble(forecastItem,"rain","3h");
                     }
                 }
 
@@ -130,7 +141,7 @@ public class FiveDayAPI extends API
         String weatherData = this.APIcall(location);
         return parseWeatherData(weatherData);
     }
-
+    
     // -------------------- Setting Latitude and Longitude
     public void setLatitude(double lati)
     {
@@ -155,26 +166,87 @@ public class FiveDayAPI extends API
         this.setLongitude(longi);
     }
 
+    //----------------------------------exception handling for double value
+    private double tryExtractDouble(JSONObject jsonObject, String key1, String key2)
+    {
+        double defaultvalue = -1.0;
+        try {
+            return jsonObject.getJSONObject(key1).getDouble(key2);
+        } catch (Exception e) {
+            return defaultvalue;
+        }
+        
+    }
+    private String tryExtractString(JSONObject jsonObject, String key) 
+    {
+        String defaultValue = "";
+        try {
+            return jsonObject.getJSONArray("weather").getJSONObject(0).getString(key);
+        } catch (Exception e) {
+            return defaultValue;
+        }
+    }
+
 }
 
 // ----------------------------------------Turns obj info into a string
 class Display5DayWeather{
 
+    //for double
+    public String checkNA(Double value)
+    {
+        if(value == -1)
+        {
+            return "N/A";
+        }
+        else
+            return String.valueOf(value);
+    }
+
+    //for int
+    public String checkNA(int value)
+    {
+        if(value == -1)
+        {
+            return "N/A";
+        }
+        else
+            return String.valueOf(value);
+    }
+
+    //for string
+    public String checkNA(String value)
+    {
+        if(value=="")
+        {
+            return "N/A";
+        }
+        else
+            return String.valueOf(value);
+    }
+    
     public String display5DaysWeather(ArrayList<WeatherInfo> obj, String location)
     {
+        
         String DisplayInfoGUI = "Five day weather: \n";
         int i = 1 ;
 
         for (WeatherInfo weather : obj) {
+            int visibility = weather.getVisibility();
+            double humidity = weather.getHumidity();
+            double feels_like = weather.getFeelsLike();
+            String desc= weather.getWeatherDescr();
+            double temperature = weather.getTemperature();
+            double rain1 = weather.getRainVol1h();
+            double rain3 = weather.getRainVol3h();
             DisplayInfoGUI += "Day" + i + ":\n"+
-                        "\nVisibility: " + weather.getVisibility() + "m\n" 
-                        + "Humidity: " + weather.getHumidity() + "%\n" 
-                        + "Feels like: " + weather.getFeelsLike() + "°C\n"
-                        + "Weather Description: " + weather.getWeatherDescr() + "\n"
-                        + "Temperature: " + weather.getTemperature() + "°C\n"
-                        + "Rain Volume (past 1h): " + weather.getRainVol1h() + "mm\n"
-                        + "Rain Volume (past 3h): " + weather.getRainVol3h() + "mm\n\n";
-
+                        "\nVisibility: " + checkNA(visibility) + "m\n" 
+                        + "Humidity: " + checkNA(humidity) + "%\n" 
+                        + "Feels like: " + checkNA(feels_like) + "°C\n"
+                        + "Weather Description: " + checkNA(desc) + "\n"
+                        + "Temperature: " + checkNA(temperature) + "°C\n"
+                        + "Rain Volume (past 1h): " + checkNA(rain1) + "mm\n"
+                        + "Rain Volume (past 3h): " + checkNA(rain3) + "mm\n\n";
             i++;
         }
 
