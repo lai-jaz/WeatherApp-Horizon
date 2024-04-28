7e0ef5950fcebcb96f5c8844285158a65a0d83eb
Laiba Ijaz <125128708+lai-jaz@users.noreply.github.com>
2024-04-28 21:35:25 +0500

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,125 +3,82 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /* Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Choose a combination:");
-        System.out.println("1. Terminal UI with SQL Database");
-        System.out.println("2. GUI with SQL Database");
-        System.out.println("3. Terminal UI with Text Database");
-        System.out.println("4. GUI with Text Database");
-        System.out.print("Enter your choice: ");
 
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                runTerminalUIWithSQL();
-                break;
-            case 2:
-                runGUIWithSQL();
-                break;
-            case 3:
-                runTerminalUIWithText();
-                break;
-            case 4:
-                runGUIWithText();
-                break;
-            default:
-                System.out.println("Invalid choice!");
-        }
-        
-        scanner.close();
-    }
-
-    private static void runTerminalUIWithSQL() {
-        // Set the classpath including terminalUI.jar and SQLdb.jar
-        String classpath = ".;libs/terminalUI.jar;libs/SQLdb.jar";
-        runApplication(classpath);
-    }
-
-    private static void runGUIWithSQL() {
-        // Set the classpath including GUI.jar and SQLdb.jar
-        String classpath = ".;libs/GUI.jar;libs/SQLdb.jar";
-        runApplication(classpath);
-    }
-
-    private static void runTerminalUIWithText() {
-        // Set the classpath including terminalUI.jar and TEXTdb.jar
-        String classpath = ".;./terminalUI.jar;./TEXTdb.jar";
-        runApplication(classpath);
-    }
-
-    private static void runGUIWithText() {
-        // Set the classpath including GUI.jar and TEXTdb.jar
-        String classpath = ".;libs/GUI.jar;libs/TEXTdb.jar";
-        runApplication(classpath);
-    }
-
-    private static void runApplication(String classpath) {
-        try {
-            // Run the application with the specified classpath
-            Process process = Runtime.getRuntime().exec("java -cp " + classpath + " Main");
-            process.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    } */ 
-    
             Scanner scanner = new Scanner(System.in);
-            
-            System.out.println("Choose a combination:");
-            System.out.println("1. Console UI with SQL Database");
-            System.out.println("2. GUI with SQL Database");
-            System.out.println("3. Console UI with Text Database");
-            System.out.println("4. GUI with Text Database");
+            int choice = 0;
+            int choicedb = 0;
+
+            //--------------------------------UI MENU
+            System.out.println("Choose the UI:");
+            System.out.println("[1] Console UI");
+            System.out.println("[2] GUI");
+            do{
+                choice = scanner.nextInt();
+            }while(choice!=1 && choice!=2);
+
+            //--------------------------------DB MENU
+            System.out.println("Choose the Database:");
+            System.out.println("[1] SQL Database");
+            System.out.println("[2] Text Database");
             System.out.print("Enter your choice: ");
+            do{
+                choicedb = scanner.nextInt();
+            }while(choicedb!=1 && choicedb!=2);
     
-            int choice = scanner.nextInt();
-    
+            DBManagerInterface db = null;
+
             switch (choice) {
-                case 1: //----------------------CONSOLE + SQL DB
-                    try{
-                        DBManagerInterface db = new DBWeatherInstance();
-                        new startAppConsole(db);
-                    }catch(Exception e)
+                case 1: //-------------------------------------------CONSOLE UI
+                     
+                    if(choicedb == 1) //--------------------SQL DB
                     {
-                        System.out.println(e.getMessage());
+                        try{
+                            db = new DBWeatherInstance();
+                        }catch(Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
                     }
-                    break;
-                case 2: //----------------------GUI + SQL DB
-                    try{
-                        DBManagerInterface db = new DBWeatherInstance();
-                        new startAppGUI(db);
-                    }catch(Exception e)
+                    else if(choicedb == 2) //----------------TEXT DB
                     {
-                        System.out.println(e.getMessage());
+                        try{
+                            db = new DB2WeatherInstance();
+                            
+                        }catch(Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
                     }
+
+                    new startAppConsole(db);
                     break;
-                case 3: //----------------------CONSOLE + TEXT DB
-                    try{
-                        DBManagerInterface db = new DB2WeatherInstance();
-                        new startAppConsole(db);
-                    }catch(Exception e)
+
+                case 2: //---------------------------------------------------GUI
+                    if(choicedb == 1) //--------------------SQL DB
                     {
-                        System.out.println(e.getMessage());
+                        try{
+                            db = new DBWeatherInstance();
+                        }catch(Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
                     }
-                    break;
-                case 4: //----------------------GUI + TEXT DB
-                    try{
-                        DBManagerInterface db = new DB2WeatherInstance();
-                        new startAppGUI(db);
-                    }catch(Exception e)
+                    else if(choicedb == 2) //----------------TEXT DB
                     {
-                        System.out.println(e.getMessage());
+                        try{
+                            db = new DB2WeatherInstance();
+                            
+                        }catch(Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
                     }
-                    break;
+                    new startAppGUI(db);
+                    break;     
                 default:
                     System.out.println("Invalid choice!");
             }
             
             scanner.close();
-    
-
     }
 }
